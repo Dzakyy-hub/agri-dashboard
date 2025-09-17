2db379b4be204d086ae9ef1f543aed42a5d6cafb
Dzaky Alfarizti <dzakyalfarizti@email.com>
2025-09-17 18:49:25 +0700

    readd picture on public

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -107,14 +107,14 @@ const AboutPage = () => {
                 {/* 1. Video Profile */}
                 <div id="video-profile" className="my-10 mx-auto max-w-3xl">
                     <div className="relative w-full overflow-hidden rounded-xl shadow-lg" style={{ paddingTop: '56.25%' }}>
-                        <iframe 
-                        width="560" height="315" 
-                        src="https://www.youtube.com/embed/d4FRslsrcgo?si=ZNS_8gvUBEJePSI7" 
-                        title="YouTube video player" 
-                        frameborder="0" 
-                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-                        referrerpolicy="strict-origin-when-cross-origin" 
-                        allowfullscreen>
+                       <iframe
+                        className="absolute top-0 left-0 w-full h-full"
+                         src="https://www.youtube.com/embed/d4FRslsrcgo?si=ZNS_8gvUBEJePSI7"
+                        title="YouTube video player"
+                        frameBorder="0" // Ganti 'frameborder' (lowercase b) menjadi 'frameBorder' (capital B)
+                        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                        referrerPolicy="strict-origin-when-cross-origin"
+                        allowFullScreen>
                         </iframe>
                     </div>
                 </div>
